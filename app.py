b09f4fa7c52be393cd4363c2a6638d10dceacb5d
Mustafa <81619569+Night-X-dev@users.noreply.github.com>
2025-08-07 14:37:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3693,7 +3693,7 @@ if __name__ == '__main__':
         scheduled_check_job,
         'cron',
         hour='14',
-        minute='32'
+        minute='38'
     )
     print("INFO: Starting scheduler...")
     scheduler.start()
