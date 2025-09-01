e007101146e7ca81c9398474aec4dc8df8f9b472
Mustafa <81619569+Night-X-dev@users.noreply.github.com>
2025-09-01 16:22:23 +0300

    c

--- app.py
+++ app.py
@@ -46,10 +46,10 @@ def serve_login_page():
     """Directs /login.html requests to the login.html page."""
     return render_template('login.html')
 
-@app.route('/personel/login.html')
-def serve_personel_login_page():
-    """Directs /personel/login.html requests to the personel/login.html page."""
-    return render_template('personel/login.html')
+@management_bp.route('/login.html')
+def login_page():
+    return render_template('login.html')  # login.html -> templates/personel/login.html
+
     
 # Yeni rota: sifremi_unuttum.html sayfasını sunar
 @app.route('/sifremi_unuttum.html')
