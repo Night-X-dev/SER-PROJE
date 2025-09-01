e282d4cc957385239f73d9a254b5556404cbabab
Mustafa <81619569+Night-X-dev@users.noreply.github.com>
2025-09-01 16:31:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -26,10 +26,9 @@ app = Flask(__name__)
 # Session management secret key
 # THIS SHOULD BE A SECURE AND UNPREDICTABLE STRING!
 app.secret_key = os.getenv("SECRET_KEY", "supersecretkeythatshouldbemorecomplex")
-from management import management_bp
+from management import management_bp  # management.py’den import ettik
 
-app.register_blueprint(management_bp, url_prefix='/personel')
-
+app.register_blueprint(management_bp)  # Blueprint’i kaydet
 
 PRIORITY_TRANSLATIONS = {
     "low": "Düşük Öncelik",
@@ -47,6 +46,11 @@ def serve_login_page():
     """Directs /login.html requests to the login.html page."""
     return render_template('login.html')
 
+@app.route('/personel/login.html')
+def serve_personel_login_page():
+    """Directs /personel/login.html requests to the personel/login.html page."""
+    return render_template('personel/login.html')
+    
 # Yeni rota: sifremi_unuttum.html sayfasını sunar
 @app.route('/sifremi_unuttum.html')
 def serve_forgot_password_page():
