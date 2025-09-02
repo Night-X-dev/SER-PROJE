623db3df7579d3563374fd967e173e9a336253dc
Mustafa <81619569+Night-X-dev@users.noreply.github.com>
2025-09-02 14:16:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4436,7 +4436,3 @@ def get_reports():
 
 #------------------------------------------------------------------ İşçi Yönetimi Bölümü ------------------------------------------------------------------
 
-@app.route('/ik_login.html')
-def serve_login_page():
-    """Directs /ik_login.html requests to the ik_login.html page."""
-    return render_template('ik_login.html')
